1f5ee93b17d3496d7741b275ffa9d5b123cdd5eb
Hunter Tidwell <wht1@rice.edu>
2016-09-09 01:57:56 -0500

    as done as it'll be

--- hw2/main.js
+++ hw2/main.js
@@ -8,13 +8,24 @@ imgs.forEach(function(name, idx) {
 function new_card() {
 	var div = document.createElement("div");
 	div.className = "card";
-	//div.innerHTML = "lorem ipsum dolor sit amet";
-	//div.innerHTML = "<img src=\"" + imgs[0] + "\" alt=\"wat\"\>";
-	var pic = document.createElement("img");
-	pic.src = imgs[0];
-	//pic.heigh = 42;
-	//pic.width = 50;
-	div.appendChild(pic);
+	if (Math.random() > 0.3) {
+		var pic = document.createElement("img");
+		var pic_num = Math.floor(imgs.length * (Math.random() * 0.99));
+		pic.src = imgs[pic_num];
+		var pic_time = 10000 * Math.random();
+		var int_num = setInterval(function() {pic.src = imgs[++pic_num % imgs.length]}, pic_time);
+		div.appendChild(pic);
+		var btn = document.createElement("input");
+		btn.type = "button";
+		btn.value = "Pause";
+		btn.onclick = function() {
+			clearInterval(int_num);
+		}
+		div.appendChild(btn);
+	}
+	var text = document.createElement("div");
+	text.innerHTML = "Lorem ipsum dolor sit amet. More latin words would make the card look better, but I\'m going for a more stream-of-consciousness vibe. Potato purple bottle verb cantancerous excellent windy old word order. Watch, we wander witlessly without. Typing nonsense makes typing code seem that much more meaningful.";
+	div.appendChild(text);
 	return div;
 }
 
@@ -28,7 +39,7 @@ function make_table_data(e) {
 function new_row() {
 	var row = document.createElement("tr");
 	row.className = "card_row";
-	for (var i = 0; i < 4; i++) {
+	for (var i = 0; i < 3; i++) {
 		row.appendChild(make_table_data(new_card()));
 	}
 	return row;
